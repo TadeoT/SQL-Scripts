b8087a6d7a8709845e36ccbc39971eb89b7df328
Tiraboschi Tadeo <tadetiraboschi@gmail.com>
2020-11-15 11:38:23 -0300

    ejercicio 4 guia 5

--- GUIA5.sql
+++ GUIA5.sql
@@ -85,3 +85,13 @@ WHILE @@fetch_status = 0
 END
 CLOSE cursorTipos
 DEALLOCATE cursorTipos
+
+--- EJERCICIO 4 ---
+
+SELECT a.au_id,a.au_fname||', '||a.au_lname as NOMBRE,a.city FROM authors a
+
+SELECT authors.au_id, publishers.city FROM authors 
+            INNER JOIN titleauthor ON authors.au_id = titleauthor.au_id
+            INNER JOIN titles ON titleauthor.title_id = titles.title_id
+            INNER JOIN publishers ON titles.pub_id = publishers.pub_id
+           
